abe9a751432b58c24777c06a61ad551cb8a53cec
krr <ab8@gmail.com>
2024-09-09 19:13:51 +0530

    Admin policy

--- MovieBookApi/Controllers/MovieStatisticsController.cs
+++ MovieBookApi/Controllers/MovieStatisticsController.cs
@@ -21,6 +21,7 @@ namespace MovieBookApi.Controllers
         }
 
         // Route for GetMovieCollections
+        [Authorize(Policy = SecurityPolicy.Admin)]
         [HttpGet("MovieCollections/{movieId}")]
         public async Task<ActionResult<List<MovieCollection>>> GetMovieCollections(string movieId)
         {
@@ -29,8 +30,8 @@ namespace MovieBookApi.Controllers
                 return NotFound(new DataTransferObject() {IsSuccess=false, Message="No collections found for the specified movie." });
             return Ok(result);
         }
-
         // Route for GetTheatreSales
+        [Authorize(Policy = SecurityPolicy.Admin)]
         [HttpGet("TheatreSales/{theatreId}")]
         public async Task<ActionResult<List<TheatreSales>>> GetTheatreSales(string theatreId)
         {
@@ -41,6 +42,7 @@ namespace MovieBookApi.Controllers
         }
 
         // Route for GetMovieOfTheMonthAsync
+        [Authorize(Policy = SecurityPolicy.Admin)]
         [HttpGet("MOM/{month}")]
         public async Task<ActionResult<uMovie>> GetMovieOfTheMonth(DateOnly month)
         {
@@ -51,6 +53,7 @@ namespace MovieBookApi.Controllers
         }
 
         // Route for GetDisasterOfTheMonthAsync
+        [Authorize(Policy = SecurityPolicy.Admin)]
         [HttpGet("DOM/{month}")]
         public async Task<ActionResult<uMovie>> GetDisasterOfTheMonth(DateOnly month)
         {
